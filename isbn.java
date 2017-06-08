a67658754b5ba506b8a6328b6dcaccafa38a0a73
Ramya97 <ramya.velusamy97@gmail.com>
2017-06-08 12:00:29 +0530

    Create isbn.java

--- isbn.java
+++ isbn.java
@@ -8,13 +8,13 @@ public static void main(String args[])
        int l=n.length();
        int n2,i=0,sum=0;
        int[] a=new int[l];
-       int n1=Integer.parseInt(n);
+       int n3=Integer.parseInt(n);
        while(n1!=0)
        {
-           n2=n1%10;
+           n2=n3%10;
            a[i]=n2;
            i++;
-           n1=n1/10;
+           n3=n3/10;
        }
        int j=0;
        for(i=1;i<l;i++)
